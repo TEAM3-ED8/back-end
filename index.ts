c3511a122b049b3922c8effd8e8ba4fbba1b5c1c
LUISEDOCCOR <luiseduardoocegueda@gmail.com>
2024-11-04 22:11:02 -0600

    fix: swagger bad config

--- index.ts
+++ index.ts
@@ -1,8 +1,8 @@
-import { app } from "./src/app";
-import swaggerSetup from "./swagger";
+import { app } from "./src/app"
+// import swaggerSetup from "./swagger";
 
-const PORT = process.env.PORT || 3000;
-swaggerSetup(app);
+const PORT = process.env.PORT || 3000
+// swaggerSetup(app)
 app.listen(PORT, () => {
-  console.log(`Servidor corriendo en puerto ${PORT}`);
-});
+  console.log(`Servidor corriendo en puerto ${PORT}`)
+})
