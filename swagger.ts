eadee329755cd9481c6ac16a8e02ae20dccfe9fd
Raydberg Gabriel Chuquival Gil <raydga_2005@hotmail.com>
2024-11-05 13:27:46 -0500

    update swagger file

--- swagger.ts
+++ swagger.ts
@@ -1,34 +1,34 @@
-// import swaggerJsDoc from 'swagger-jsdoc';
-// import swaggerUi from 'swagger-ui-express';
-// import { Express } from 'express';
+import swaggerJsDoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
+import { Express } from 'express';
 
-// const options = {
-//   swaggerDefinition: {
-//     openapi: '3.0.0',
-//     info: {
-//       title: 'Inventory System API',
-//       version: '1.0.0',
-//       description: '',
-//     },
-//     servers: [
-//       {
-//         url: 'http://localhost:3000/api',
-//       },
-//     ],
-//   },
-//   apis: ['./openapi.yaml'],
-// };
+const options = {
+  swaggerDefinition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Inventory System API',
+      version: '1.0.0',
+      description: '',
+    },
+    servers: [
+      {
+        url: 'http://localhost:3000/api',
+      },
+    ],
+  },
+  apis: ['./openapi.yaml'],
+};
 
-// const specs = swaggerJsDoc(options);
+const specs = swaggerJsDoc(options);
 
-// const swaggerSetup = (app: Express) => {
-//   app.use(
-//     '/api-docs',
-//     swaggerUi.serve,
-//     swaggerUi.setup(specs, {
-//       tryItOutEnabled: true,
-//     })
-//   );
-// };
+const swaggerSetup = (app: Express) => {
+  app.use(
+    '/api-docs',
+    swaggerUi.serve,
+    swaggerUi.setup(specs, {
+      tryItOutEnabled: true,
+    })
+  );
+};
 
-// export default swaggerSetup;
+export default swaggerSetup;
