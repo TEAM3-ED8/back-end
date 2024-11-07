3712f92f6915cd09f257d72d2278c9af140be446
Pedro Purihuaman <pedropuriihuaman@gmail.com>
2024-11-06 23:52:53 -0500

    feat: implement CRUD operations for addresses

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ export const app = express()
 import { createRangeRouter } from "./routes/range.routes"
 import { createElveRouter } from "./routes/elve.routes"
 import { createReindeerRouter } from "./routes/reindeer.routes"
+import { createAddressRouter } from "./routes/address.routes"
 
 app.use(express.json())
 
@@ -11,14 +12,19 @@ app.get("/", (req, res) => {
 })
 
 // routes
-app.use("/api/range", (req, res, next) => {
-  console.log("Accediendo a /api/range")
-  next()
-}, createRangeRouter())
+
+app.use(
+  "/api/range",
+  (req, res, next) => {
+    console.log("Accediendo a /api/range")
+    next()
+  },
+  createRangeRouter()
+)
 
 app.use("/api/elve", createElveRouter())
 app.use("/api/reindeer", createReindeerRouter())
-
+app.use("/api/address", createAddressRouter())
 
 app.use((err: any, req: Request, res: Response, next: NextFunction) => {
   console.error(err.stack)
---
+++ src/controllers/AddressesController.ts
@@ -0,0 +1,65 @@
+import type { Addresses } from "@prisma/client"
+import type { Request, Response } from "express"
+import {
+  createAddress,
+  deleteAddress,
+  getAllAddress,
+  getByIdAddress,
+  updateAddress
+} from "../models/AddressesModel"
+
+export const getAll = async (req: Request, res: Response) => {
+  try {
+    const allAddresses: Addresses[] = await getAllAddress()
+    res.json(allAddresses)
+  } catch (error) {
+    res.sendStatus(500)
+  }
+}
+
+export const getById = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params
+    const address: Addresses = await getByIdAddress({ id: Number(id) })
+
+    if (!address) {
+      res.status(404).json({ msg: "Address not found" })
+      return
+    }
+    res.json(address)
+  } catch (error) {
+    res.sendStatus(500)
+  }
+}
+
+export const create = async (req: Request, res: Response) => {
+  try {
+    const address: Addresses = req.body
+    const createdAddress: Addresses = await createAddress(address)
+    res.json(createAddress)
+  } catch (error) {
+    res.sendStatus(500)
+  }
+}
+
+export const update = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params
+    const address: Addresses = req.body
+
+    const updatedAddress: Addresses = await updateAddress(Number(id), address)
+    res.json(updatedAddress)
+  } catch (error) {
+    res.status(500).json({ error: "Error updating address" })
+  }
+}
+
+export const remove = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params
+    await deleteAddress({ id: Number(id) })
+    res.sendStatus(204)
+  } catch (error) {
+    res.sendStatus(500)
+  }
+}
