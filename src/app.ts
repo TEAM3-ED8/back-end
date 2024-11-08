361a9c44c1ec89e15c89ca9ef4adea7bdb435f89
Raydberg Gabriel Chuquival Gil <raydga_2005@hotmail.com>
2024-11-07 23:02:09 -0500

    feat: integrar swagger-jsdoc y swagger-ui-express para documentación de la API

--- src/app.ts
+++ src/app.ts
@@ -4,10 +4,25 @@ import { createRangeRouter } from "./routes/range.routes"
 import { createElveRouter } from "./routes/elve.routes"
 import { createReindeerRouter } from "./routes/reindeer.routes"
 import { corsMiddleware } from "./middlewares/cors"
-import swaggerSetup from "../swagger"
+import swaggerJsDoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
+
 app.use(corsMiddleware)
 app.use(express.json())
-swaggerSetup(app)
+const swaggerOptions = {
+  swaggerDefinition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Elves API',
+      version: '1.0.0',
+      description: 'API para gestionar elfos',
+    },
+  },
+  apis: ['./routes/elve.routes.ts'],
+};
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
 app.get("/", (req, res) => {
   res.json({ message: "API Funcionando!" })
 })
--- swagger.ts
+++ swagger.ts
@@ -1,37 +1,28 @@
-import swaggerUi from "swagger-ui-express"
-import swaggerJsDoc from "swagger-jsdoc"
-import { Express } from "express"
-import path from "path"
+// import swaggerUi from 'swagger-ui-express';
+// import swaggerJsDoc from 'swagger-jsdoc';
+// import { Express } from 'express';
+// import path from 'path';
+// const isProduction = process.env.NODE_ENV === 'production';
 
-const isProduction = process.env.NODE_ENV === "production"
+// const options = {
+//   definition: {
+//     openapi: '3.0.0',
+//     info: {
+//       title: 'Inventory System API',
+//       version: '1.0.0',
+//       description: '',
+//     },
+//     servers: [
+//       {
+//         url: isProduction ? process.env.PROD_URL : process.env.DEV_URL,
+//       },
+//     ],
+//   },
+//   apis: [path.join(__dirname, '/src/routes/*.ts')],
+// };
 
-const options = {
-  definition: {
-    openapi: "3.0.0",
-    info: {
-      title: "Inventory System API",
-      version: "1.0.0",
-      description: ""
-    },
-    servers: isProduction
-      ? [
-          {
-            url: process.env.PROD_URL,
-            description: "Producción"
-          }
-        ]
-      : [
-          {
-            url: process.env.DEV_URL,
-            description: "Desarrollo"
-          }
-        ]
-  },
-  apis: [path.join(__dirname, "/src/routes/*.ts")]
-}
+// const swaggerSpec = swaggerJsDoc(options);
 
-const swaggerSpec = swaggerJsDoc(options)
-
-export default (app: Express) => {
-  app.use("/api/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec))
-}
+// export default (app: Express) => {
+//     app.use('/api/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+//   };
