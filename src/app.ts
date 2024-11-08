73c6fb83237920a4516e575536ce4abb124b2b15
Raydberg Gabriel Chuquival Gil <raydga_2005@hotmail.com>
2024-11-07 20:51:12 -0500

    fix: config swagger

--- src/app.ts
+++ src/app.ts
@@ -4,22 +4,26 @@ import { createRangeRouter } from "./routes/range.routes"
 import { createElveRouter } from "./routes/elve.routes"
 import { createReindeerRouter } from "./routes/reindeer.routes"
 import { corsMiddleware } from "./middlewares/cors"
+import swaggerSetup from "../swagger"
 app.use(corsMiddleware)
 app.use(express.json())
-
+swaggerSetup(app)
 app.get("/", (req, res) => {
   res.json({ message: "API Funcionando!" })
 })
 
 // routes
-app.use("/api/range", (req, res, next) => {
-  console.log("Accediendo a /api/range")
-  next()
-}, createRangeRouter())
+app.use(
+  "/api/range",
+  (req, res, next) => {
+    console.log("Accediendo a /api/range")
+    next()
+  },
+  createRangeRouter()
+)
 
 app.use("/api/elve", createElveRouter())
 app.use("/api/reindeer", createReindeerRouter())
-
 
 app.use((err: any, req: Request, res: Response, next: NextFunction) => {
   console.error(err.stack)
