2c00b3e47c31812e3bab5b1f2ab1197147feaea3
Pedro Purihuaman <91698863+puriihuaman@users.noreply.github.com>
2024-11-09 11:54:40 -0500

    Revert "Endpoint address (crud)"

--- index.ts
+++ index.ts
@@ -1,7 +1,6 @@
-import { app } from "./src/app"
-// import swaggerSetup from "./swagger";
-const PORT = process.env.PORT || 3000
-// swaggerSetup(app)
+import { app } from "./src/app";
+
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Servidor corriendo en puerto ${PORT}`)
-})
+  console.log(`Servidor corriendo en puerto ${PORT}`);
+});
--- src/app.ts
+++ src/app.ts
@@ -1,32 +1,8 @@
-import express, { Request, Response, NextFunction } from "express"
-export const app = express()
-import { createRangeRouter } from "./routes/range.routes"
-import { createElveRouter } from "./routes/elve.routes"
-import { createReindeerRouter } from "./routes/reindeer.routes"
-import { createAddressRouter } from "./routes/address.routes"
+import express from "express";
+export const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.json({ message: "API Funcionando!" })
-})
-
-// routes
-
-app.use(
-  "/api/range",
-  (req, res, next) => {
-    console.log("Accediendo a /api/range")
-    next()
-  },
-  createRangeRouter()
-)
-
-app.use("/api/elve", createElveRouter())
-app.use("/api/reindeer", createReindeerRouter())
-app.use("/api/address", createAddressRouter())
-
-app.use((err: any, req: Request, res: Response, next: NextFunction) => {
-  console.error(err.stack)
-  res.status(500).json({ error: "Internal Server Error" })
-})
+  res.json({ message: "API Funcionando!" });
+});
