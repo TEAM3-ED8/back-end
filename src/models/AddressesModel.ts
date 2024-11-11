44dfc16b3212dafae26c0dcd0ad7e43ca68a0f79
Pedro Purihuaman <pedropuriihuaman@gmail.com>
2024-11-11 12:21:53 -0500

    fix: update parameter names

--- src/models/AddressesModel.ts
+++ src/models/AddressesModel.ts
@@ -5,7 +5,7 @@ export const getAllAddress = async () => {
   return await prisma.addresses.findMany()
 }
 
-export const getByIdAddress = async ({ id }: { id: number }) => {
+export const getByIdAddress = async ({ id }: Addresses["id"]) => {
   return await prisma.addresses.findUnique({ where: { id } })
 }
 
@@ -13,13 +13,16 @@ export const createAddress = async (address: Addresses) => {
   return await prisma.addresses.create({ data: address })
 }
 
-export const updateAddress = async (id: number, address: Addresses) => {
+export const updateAddress = async (
+  id: Addresses["id"],
+  address: Addresses
+) => {
   return await prisma.addresses.update({
     where: { id },
     data: address
   })
 }
 
-export const deleteAddress = async ({ id }: { id: number }) => {
+export const deleteAddress = async ({ id }: Addresses["id"]) => {
   return await prisma.addresses.delete({ where: { id } })
 }
